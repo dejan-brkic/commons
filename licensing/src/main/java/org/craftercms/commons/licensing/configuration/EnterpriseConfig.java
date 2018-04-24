20cd231ebda8e0ce0441736646de869ffe2d3eb6
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-04-24 19:29:21 +0200

    work in progress

--- licensing/src/main/java/org/craftercms/commons/licensing/configuration/EnterpriseConfig.java
+++ licensing/src/main/java/org/craftercms/commons/licensing/configuration/EnterpriseConfig.java
@@ -2,17 +2,40 @@ package org.craftercms.commons.licensing.configuration;
 
 import org.craftercms.commons.licensing.LicenseValidator;
 import org.craftercms.commons.licensing.SimpleLicenseValidator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @Profile("enterprise")
+@PropertySources(value = { @PropertySource("classpath:org/craftercms/commons/licensing/license-validator.properties"),
+    @PropertySource("classpath:*crafter/licensing/license-validator.properties") })
 public class EnterpriseConfig {
+
+    @Value("${crafter.license.location")
+    String licenseLocation;
+
+    @Value("${crafter.license.keyLocation")
+    String keyLocation;
+
+    @Value("${crafter.license.password")
+    String password;
+
+    @Bean
+    static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean(name = "crafter.licenseValidator")
     public LicenseValidator licenseValidator() {
-        LicenseValidator licenseValidator = new SimpleLicenseValidator();
+        SimpleLicenseValidator licenseValidator = new SimpleLicenseValidator();
+        licenseValidator.setLicenseLocation(licenseLocation);
+        licenseValidator.setKeyLocation(keyLocation);
+        licenseValidator.setPassword(password);
         return licenseValidator;
     }
 }
